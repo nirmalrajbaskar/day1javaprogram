70408ab7b5bd27b8263d621c9d7d83bd597f2bc6
91637 <91637@LAPTOP-NVE6P1A9>
2024-07-01 06:22:15 +0530

    "day 1 program of javabasics"

---
+++ java_day1/src/java_day1_package/day1_class.java
@@ -0,0 +1,11 @@
+package java_day1_package;
+
+public class day1_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO TNSIF");
+
+	}
+
+}
