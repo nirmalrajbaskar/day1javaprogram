acc5b17edb02e6f82f8f537887b9d855dcfee859
91637 <91637@LAPTOP-NVE6P1A9>
2024-07-01 11:44:39 +0530

    "day2-loop and day3-encapsulation"

---
+++ java_day1/src/day_2/Decisionmakingoperators.java
@@ -0,0 +1,25 @@
+package day_2;
+
+public class Decisionmakingoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=8,y=9;
+		int a = 10;
+		int b = 11;
+		if (x >= y) {
+		System.out.println("true");
+		} else {
+		System.out.println("false");
+		}
+		if (!(a < b) || (a == b)) {
+		System.out.println("Condition is TRUE");
+		} else
+		{
+		System.out.println("Condition is FALSE");
+		}
+
+
+	}
+
+}
---
+++ java_day1/src/day_3/Encaps_eg1.java
@@ -0,0 +1,16 @@
+package day_3;
+
+public class Encaps_eg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encaps2 obj = new Encaps2();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		}
+
+
+	}
+
